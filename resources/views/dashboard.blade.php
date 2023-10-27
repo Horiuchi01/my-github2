d5b04124aced3020b76ce4a7274c097cfb630359
horiuchi01 <horiuchi@aska-ltd.jp>
2023-10-27 18:01:50 +0900

    weatherMap追加

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -33,165 +33,53 @@
             const hour = today.getHours();
             const todayDate = `${year}-${month}-${day}T${hour}:00`;
 
-            // data.hourlyが存在するかを一度だけチェック
+            const weatherMap = {
+                "Unknown": "晴天",
+                1: "晴れ",
+                2: "時々曇り",
+                3: "曇り",
+                45: "霧",
+                48: "霧氷",
+                51: "軽い霧雨",
+                53: "霧雨",
+                55: "濃い霧雨",
+                56: "氷結霧雨",
+                57: "濃い氷結霧雨",
+                61: "小雨",
+                63: "雨",
+                65: "大雨",
+                66: "凍てつく雨",
+                67: "激しい凍てつく雨",
+                71: "小雪",
+                73: "雪",
+                75: "大雪",
+                77: "ひょう",
+                80: "軽いにわか雨",
+                81: "にわか雨",
+                82: "激しいにわか雨",
+                85: "降雪",
+                86: "激しい降雪",
+                95: "雷雨",
+                96: "ひょうを伴う雷雨",
+                99: "ひょうを伴う激しい雷雨",
+            };
+
             if (data.hourly && data.hourly.weathercode) {
                 for(let i = 0; i < data.hourly.weathercode.length; i++) {
                     const weatherCode = data.hourly.weathercode[i] || "Unknown"; 
                     const weatherDate = data.hourly.time[i] || "Unknown";
+
                     if (weatherDate == todayDate) {
-                        switch (weatherCode){
-                            case "Unknown":
-                                weatherType = "晴天";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 1:
-                                weatherType = "晴れ";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 2:
-                                weatherType = "時々曇り";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 3:
-                                weatherType = "曇り";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 45:
-                                weatherType = "霧";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 48:
-                                weatherType = "霧氷";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 51:
-                                weatherType = "軽い霧雨";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 53:
-                                weatherType = "霧雨";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 55:
-                                weatherType = "濃い霧雨";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 56:
-                                weatherType = "氷結霧雨";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 57:
-                                weatherType = "濃い氷結霧雨";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 61:
-                                weatherType = "小雨";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 63:
-                                weatherType = "雨";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 65:
-                                weatherType = "大雨";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 66:
-                                weatherType = "凍てつく雨";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 67:
-                                weatherType = "凍てつく大雨";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 71:
-                                weatherType = "小雪";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 73:
-                                weatherType = "雪";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 75:
-                                weatherType = "大雪";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 77:
-                                weatherType = "ひょう";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 80:
-                                weatherType = "軽いにわか雨";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 81:
-                                weatherType = "にわか雨";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 82:
-                                weatherType = "激しいにわか雨";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 85:
-                                weatherType = "軽い雪";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 86:
-                                weatherType = "激しい雪";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 95:
-                                weatherType = "雷雨";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 96:
-                                weatherType = "ひょうを伴う雷雨";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            case 99:
-                                weatherType = "ひょうを伴う激しい雷雨";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                                break;
-                            default:
-                                weatherType = "不明";
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
-                                header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
-                        }
-                    }
-                    
+                        const weatherType = weatherMap[weatherCode] || "不明";
+                        header.insertAdjacentHTML('beforebegin', `<p>${weatherType}</p>`);
+                        header.insertAdjacentHTML('beforebegin', `<p>${weatherDate}</p>`);
+                    } 
                 }
             }
         } catch (error) {
             console.log(`エラーが発生しました: ${error.message}`);
         }
     }
+
     fetchWeather();
 </script>
