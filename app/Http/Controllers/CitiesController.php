848a0ab4b301362d67d54953105a9d389a3c6bc3
horiuchi01 <horiuchi@aska-ltd.jp>
2023-10-31 12:34:22 +0900

    変更

--- app/Http/Controllers/CitiesController.php
+++ app/Http/Controllers/CitiesController.php
@@ -9,11 +9,8 @@ class CitiesController extends Controller
 {
     public function dashboard()
     {
-        // 現在ログインしているユーザーのIDを取得
         $userId = Auth::id();
 
-        // リレーションを使用して、関連するusersテーブルのデータも取得
-        // ただし、現在ログインしているユーザーに関連するデータのみを取得
         $cities = City::whereHas('users', function ($query) use ($userId) {
             $query->where('id', $userId);
         })->with('users')->get();
